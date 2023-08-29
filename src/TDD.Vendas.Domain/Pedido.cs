392548db866966257b086c6c3ae318b814cb3e2b
Edson Santos <dev@escode.net>
2023-08-29 05:42:10 -0300

    Teste para: Adicionar Item Pedido Existente

--- src/TDD.Vendas.Domain/Pedido.cs
+++ src/TDD.Vendas.Domain/Pedido.cs
@@ -2,15 +2,57 @@
 
 public class Pedido
 {
-    public decimal ValorTotal { get; set; }
-    
+    public decimal ValorTotal { get; private set; }
+    public PedidoStatus Status { get; private set; }
+    public Guid ClienteId { get; private set; }
+
     private readonly List<PedidoItem> _pedidoItems = new();
     public IReadOnlyCollection<PedidoItem> PedidoItems => _pedidoItems;
 
+
+    protected Pedido() { }
+
+
+    public void AtualizarValorTotal()
+    {
+        ValorTotal = PedidoItems.Sum(p => p.CalcularValor());
+    }
+
     public void AdicionarItemPedido(PedidoItem pedidoItem)
     {
+        if (_pedidoItems.Any(p => p.Id == pedidoItem.Id))
+        {
+            var pedidoItemExistente = _pedidoItems.FirstOrDefault(p => p.Id == pedidoItem.Id);
+
+            pedidoItemExistente.AdicionarQuantidade(pedidoItem.Quantidade);
+
+            pedidoItem = pedidoItemExistente;
+
+            _pedidoItems.Remove(pedidoItemExistente);
+        }
+
         _pedidoItems.Add(pedidoItem);
-        ValorTotal = PedidoItems.Sum(p => p.Valor * p.Quantidade);
+        AtualizarValorTotal();
+    }
+
+    public void TornarRascunho()
+    {
+        Status = PedidoStatus.Rascunho;
+    }
+
+    public static class PedidoFactory
+    {
+        public static Pedido NovoPedidoRascunho(Guid clienteId)
+        {
+            var pedido = new Pedido
+            {
+                ClienteId = clienteId
+            };
+
+            pedido.TornarRascunho();
+
+            return pedido;
+        }
     }
 }
 
@@ -19,13 +61,28 @@ public class PedidoItem
     public Guid Id { get; private set; }
     public string NomeProduto { get; private set; }
     public int Quantidade { get; private set; }
-    public decimal Valor { get; private set; }
+    public decimal ValorUnitario { get; private set; }
 
-    public PedidoItem(Guid id, string nomeProduto, int quantidade, decimal valor)
+
+    public PedidoItem(Guid id, string nomeProduto, int quantidade, decimal valorUnitario)
     {
         Id = id;
         NomeProduto = nomeProduto;
         Quantidade = quantidade;
-        Valor = valor;
+        ValorUnitario = valorUnitario;
     }
+
+
+    internal void AdicionarQuantidade(int quantidade) => Quantidade += quantidade;
+
+    internal decimal CalcularValor() => Quantidade * ValorUnitario;
+}
+
+public enum PedidoStatus
+{
+    Rascunho = 100,
+    Iniciado = 200,
+    Pago = 300,
+    Entregue = 400,
+    Cancelado = 500
 }
--- tests/TDD.Vendas.Domain.Tests/PedidoTests.cs
+++ tests/TDD.Vendas.Domain.Tests/PedidoTests.cs
@@ -4,10 +4,10 @@ public class PedidoTests
 {
     [Fact(DisplayName = "Adicionar Item Novo Pedido")]
     [Trait("Categoria", "Pedido Tests")]
-   public void AdicionarItemPedido_NovoPedido_DeveAtualizarValor()
+    public void AdicionarItemPedido_NovoPedido_DeveAtualizarValor()
     {
         // Arrange
-        var pedido = new Pedido();
+        var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(Guid.NewGuid());
         var pedidoItem = new PedidoItem(Guid.NewGuid(), "Pedido Item Teste", 2, 100);
 
 
@@ -17,5 +17,25 @@ public class PedidoTests
 
         //Assert
         Assert.Equal(200, pedido.ValorTotal);
+    }
+
+    [Fact(DisplayName = "Adicionar Item Pedido Existente")]
+    [Trait("Categoria", "Pedido Tests")]
+    public void AdicionarItemPedido_ItemExistente_DeveIncrementarUnidadesSomarValores()
+    {
+        // Arrange
+        var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(Guid.NewGuid());
+        var produtoId = Guid.NewGuid();
+        var pedidoItem = new PedidoItem(produtoId, "Pedido Item Teste", 2, 100);
+        pedido.AdicionarItemPedido(pedidoItem);
+        var pedidoItem2 = new PedidoItem(produtoId, "Pedido Item Teste", 2, 100);
+
+        //Act
+        pedido.AdicionarItemPedido(pedidoItem2);
+
+        //Assert
+        Assert.Equal(400, pedido.ValorTotal);
+        Assert.Equal(1, pedido.PedidoItems.Count);
+        Assert.Equal(4, pedido.PedidoItems.FirstOrDefault(p => p.Id == produtoId).Quantidade);
     }
 }
